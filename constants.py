4e271276000e5d687a9bb8330c2b9c8883993922
Sanhita Joshi <sanhita.joshi@gmail.com>
2025-11-12 19:44:30 -0600

    notebook and constants files edited

--- constants.py
+++ constants.py
@@ -1,22 +1,19 @@
 # Save the files downloaded from zenodo in `DATA_INPUT_DIR`
-DATA_INPUT_DIR = 'data'
+DATA_DIR = '../data'
+MODEL_DIR = '../model'
 
-FILE_NUM = 16  # the file number to read
+N_LEADS = 12  # 12 channels for ECG
+N_TOTAL = 0  # make this 0 to use filters
+RECONSTRUCT = False  # To create a file with the reconstructed traces
+
+OUTPUT_PREDICTIONS = f'{DATA_DIR}/predictions.csv'
+RECONSTRUCT_FILE = f'{DATA_DIR}/reconstructed_traces.npy'
+
 # using 16 as default - that has the most data with the following 2 constraints:
 # ABS_AGE_DIFF and NORMAL_ECG
 
 ABS_AGE_DIFF = 1  # the absolute difference between chronological and predicted ages
-NORMAL_ECG = True
+# NORMAL_ECG = True
 
-AGE_FILTER = False  # using patients with this chronological age.
+# AGE_FILTER = False  # using patients with this chronological age.
 
-DATA_OUTPUT_DIR = f'output/p{FILE_NUM}'  # where reconstrctured ECG files would be saved.
-
-PREDICTED_AGE_CSV = f'{DATA_OUTPUT_DIR}/prediction.csv'
-# saved only if KEEP_AGE = True
-
-batch_size = 8
-traces_dset = 'tracings'  # used in reading hdf5 file.
-N_LEADS = 12  # 12 channels for ECG
-SAVE_HDF5 = False  # use this if working with "a lot of" patients.
-EXAM_ID = 'exam_id'
--- evaluate_script.py
+++ evaluate_script.py
@@ -8,13 +8,18 @@ import torch.optim as optim
 import tqdm
 
 from constants import (
-    DATA_INPUT_DIR,
+    ABS_AGE_DIFF,
+    DATA_DIR,
+    MODEL_DIR,
     N_LEADS,
+    N_TOTAL,
+    RECONSTRUCT,
+    RECONSTRUCT_FILE,
 )
 from resnet import ResNet1d
 from train import compute_loss, compute_weights
 
-config = './model/config.json'
+config = f'{MODEL_DIR}/config.json'
 
 # Instantiate the model using the config.json information.
 with open(config, 'r') as f:
@@ -30,7 +35,7 @@ model = ResNet1d(
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 # Retrieve the state dict, which has all the coefficients
-state_dict = (torch.load('./model/model.pth',
+state_dict = (torch.load(f'{MODEL_DIR}/model.pth',
               weights_only=False,
               map_location=device))
 
@@ -44,9 +49,9 @@ def predict(
         df,
         exam_ids,
         reconstruct=False,
-        n_total=0,
         batch_size=10,
 ):
+    n_total = N_TOTAL
     if n_total == 0:
         n_total = len(data_array)
     n_batches = int(np.ceil(n_total/batch_size))
@@ -91,11 +96,14 @@ def predict(
         pred_list.append(preds)
 
     preds = pd.concat(pred_list, axis=0, ignore_index=True)
-    compare = df.merge(preds, on='exam_id', how='inner')
+    preds = df.merge(preds, on='exam_id', how='inner')
+
+    preds.to_csv(f"{DATA_DIR}/prediction.csv", index=False)
+
     if reconstruct:
         recon_traces = np.concatenate(reconstructed_traces, axis=0)
         print(f"recon_traces.shape: {recon_traces.shape}")
-        np.save("reconstructed_traces.npy", recon_traces)
+        np.save(RECONSTRUCT_FILE, recon_traces)
         plt.plot(recon_traces[0, :, 0], label='Reconstructed')
         plt.plot(data_array[0, :, 0], label='original')
         plt.legend()
@@ -103,20 +111,23 @@ def predict(
 
     # Plot the new predictions against the metadata predictions
 
-    plt.scatter(compare['nn_predicted_age'], compare['torch_pred'])
+    plt.scatter(preds['nn_predicted_age'], preds['torch_pred'])
     plt.xlabel('NN Predicted Age')
     plt.ylabel('Torch Predicted Age')
     plt.savefig("plot.png")
     plt.show()
+    return
 
 
-def main(n_total=100):
+def main():
+    n_total = N_TOTAL
+
     # Read in exam metadata and limit to file 16.
-    df = pd.read_csv(f'./{DATA_INPUT_DIR}/exams.csv')
+    df = pd.read_csv(f'{DATA_DIR}/exams.csv')
     df = df[df['trace_file'] == 'exams_part16.hdf5']
 
     # Read in raw ECG data for file 16.
-    filename = "./data/exams_part16.hdf5"
+    filename = f"{DATA_DIR}/exams_part16.hdf5"
 
     with h5py.File(filename, "r") as f:
         print("Keys in the HDF5 file:", list(f.keys()))
@@ -130,7 +141,7 @@ def main(n_total=100):
 
     if n_total == 0:
         df = df[
-            (abs(df['nn_predicted_age'] - df['age']) < 1) &
+            (abs(df['nn_predicted_age'] - df['age']) < ABS_AGE_DIFF) &
             (df['normal_ecg'])
         ].copy()
 
@@ -142,11 +153,13 @@ def main(n_total=100):
         data_array = data_array[mask, :, :]  # shape: (len(indices), ...)
         n_total = mask.sum()
 
+    else:
+        data_array = data_array[:n_total]
+
     return data_array, df, exam_ids
 
 
 if __name__ == "__main__":
-    n_total = 100  # to use filters; use a positive number to use first n
     batch_size = 20
-    data_array, df, exam_ids = main(n_total=n_total)
-    predict(data_array, df, exam_ids, reconstruct=True, n_total=n_total, batch_size=batch_size)
+    data_array, df, exam_ids = main()
+    predict(data_array, df, exam_ids, reconstruct=RECONSTRUCT, batch_size=batch_size)
