1476658c567e81854412161a64ef7a7ae6eb666f
Sanhita Joshi <sanhita.joshi@gmail.com>
2026-01-14 19:38:26 -0600

    fixing function params and notebook example

--- evaluate_script.py
+++ evaluate_script.py
@@ -219,6 +219,8 @@ def calculate_removal_error(
     :param interval: how many pixels to remove
     :param total_subjects: use first n values of the array for predictions; if 0 means use the whole array
     :param n_idx: go from start to end with n_idx in the range function
+    :param replace_near: if true use the last unremoved value for replacement; if false use average of
+    before and after values of the removed patch
     """
 
     data_array = np.load(data_array_loc)
